8377deb3eae77b70ceb0f48d5b348fc1eb579b9f
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-03-28 23:12:39 -0700

    finish 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let currYear = (new Date()).getFullYear();
+
+
+
+    // determine age
+    let person = people.forEach(function (person) {
+
+        // if someone doesn't have a yearOfDeath, use currYear to find age
+        if(person['yearOfDeath'] === undefined) {
+            person['age'] = currYear - person['yearOfBirth'];
+        } else {
+            person['age'] = person['yearOfDeath'] - person['yearOfBirth'];
+        }
+        console.table(people);
+
+    })
+
+    
+    // sort from oldest to youngest
+    let ordered = people.sort(function(person1, person2) {
+        if (person1.age > person2.age) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+    console.table(ordered);
+
+    return ordered[0];
 
 };
 
