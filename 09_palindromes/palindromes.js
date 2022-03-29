7e2275f60dd98f781440a0ab58cce9e63be779c9
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-03-28 21:43:44 -0700

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+    let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~\ ';
+
+    // remove punctuation
+    text = text.toLowerCase();
+    let rawLetters = text.split('');
+    let cleanLetters = rawLetters.filter(function(letter) {
+        return punctuation.indexOf(letter) === -1;
+    })
+    console.log(cleanLetters);
+
+    for (let i = 0; i < Math.floor(cleanLetters.length / 2); i++) {
+        let leftLetter = cleanLetters[i]
+        let rightLetter = cleanLetters[cleanLetters.length-1-i]
+        if (leftLetter !== rightLetter) {
+            return false;
+        }
+    }
+    return true;
+
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
