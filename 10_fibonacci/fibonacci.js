4a1f150b3aad628ccbbc2240e981eb2a799d99c6
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-03-28 22:09:48 -0700

    finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+
+    if (fibNum < 0) {
+        return 'OOPS';
+    }
+
+    fibNum = parseInt(fibNum);
+
+    return fibMath(fibNum);
 
 };
+
+function fibMath(fibNum) {
+
+    if (fibNum == 0) {
+        return 0;
+    } else if (fibNum == 1) {
+        return 1;
+    } else {
+        return ( fibMath(fibNum-1) + fibMath(fibNum-2));
+    }
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
