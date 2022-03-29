d24335904be4ef7e72cde3b7915079b43bcd4ea8
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-03-28 21:22:14 -0700

    finish 08_calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+	
+  let sum = 0;
+  arr.forEach(item => {
+    sum += item;
+  });
+  return sum;
+};
+
+const multiply = function(arr) {
+  let total = 1;
+
+  arr.forEach(item => {
+    total *= item;
+  });
+  return total;
+};
+
+const power = function(a,b) {
+
+  return a ** b;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(num) {
+	let final = 1;
+  for (let i=num; i>0; i--){
+    final *= i;
+  }
+  return final;
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
